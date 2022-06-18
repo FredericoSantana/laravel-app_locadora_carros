d14f8055429d2b24bea5459abf0063e4f70db3b0
FredericoSantana <fredericosantana@hotmail.com.br>
2022-06-18 09:05:19 -0300

    refactoring do endpoint update de marca e modelo

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -137,10 +137,18 @@ class MarcaController extends Controller
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens', 'public');
 
+        //preencher o objeto $marca com os dados do request
+        $marca->fill($request->all());
+        $marca->imagem = $imagem_urn;
+//        dd($marca->getAttributes());
+        $marca->save();
+        /*
         $marca->update([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
         ]);
+        */
+
         return response()->json($marca, 200);
     }
 
