d0896515efd78b2403dbc8dadaabb28b91aa098f
FredericoSantana <fredericosantana@hotmail.com.br>
2022-06-27 22:21:24 -0300

    Criando a rota, a view e o componente marcas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,11 @@
                             <a class="nav-link" href="#">Locações</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Veículos</a>
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Veículos</a>
                             <div class="dropdown-menu">
                                 <a class="dropdown-item" href="#">Carros</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Marcas</a>
+                                <a class="dropdown-item" href="{{ route('marcas') }}">Marcas</a>
                                 <a class="dropdown-item" href="#">Modelos</a>
                             </div>
                         </li>
